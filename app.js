03bee670de375be8c28c81d272f87a2ac5349750
ShivanG <shivangramola007@gmail.com>
2023-11-20 13:20:45 +0530

    final update

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const form = document.querySelector("form");
+const reset = document.querySelector(".clear");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
-  let qr = document.querySelector(".qr").value;
+  let qr = document.querySelector(".qr");
   let qrCodeContainer = document.querySelector(".qr-code");
-  qrCodeContainer.innerHTML = "";
-  let qrs = new QRious({
-    element: qrCodeContainer,
-    value: qr,
-    size: 300,
-  });
+  let empty = document.querySelector(".empty");
+  let data = document.querySelector(".data");
+  if (qr.value === "") {
+    empty.innerHTML = `input box is empty :)`;
+  } else {
+    data.innerHTML = "QR Code :-";
+    let qrs = new QRious({
+      element: qrCodeContainer,
+      value: qr.value,
+      size: 300,
+    });
+  }
+  qr.value = "";
+});
+
+reset.addEventListener("click", function () {
+  let data = document.querySelector(".data");
+  data.innerHTML = "";
+  let qrCodeContainer = document.querySelector(".qr-code");
+  qrCodeContainer.remove();
 });
